20c756427ac0f697e73e4e0aad8960ed1bab8248
Blaine Pace <blainepace@gmail.com>
2012-01-02 23:25:46 -0800

    Fixed specs so that they're at least passing. Test suite is still abysmal.

--- spec/movies_spec.rb
+++ spec/movies_spec.rb
@@ -11,17 +11,17 @@ describe "Movies" do
   describe "fail" do
 
     it "should return a nil object" do
-      @moviemgr.lookup("asdfasdfasdf").should be_empty
+      @moviemgr.lookup("asdfasdfasdf").should be_nil
     end
   end
 
   describe "success" do
     it "should handle a filesystem path gracefully" do
-      @moviemgr.lookup("testing/paths/Inception").class.should == Array
+      @moviemgr.lookup("testing/paths/Inception").class.should == Hash
     end
 
-    it "should return an array of matches" do
-      @moviemgr.lookup("Inception").class.should == Array
+    it "should return the best match as a hash" do
+      @moviemgr.lookup("Inception").class.should == Hash
     end
   end
 end
